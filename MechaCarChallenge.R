695310933714db8b813209ae66b71c7868cf2b02
Jose Calucag <jose.calucag@outlook.com>
2020-12-04 04:44:33 -0500

    adding Challenge Deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -12,3 +12,13 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 # Perform summary
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar))
 
+# Deliverable 2 ------------------------
+
+# Read Suspended_Coil.csv
+SusCoil <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors=F)
+
+# Create total_summary dataframe
+total_summary <- SusCoil %>% summarize(Mean=mean(PSI), Median=median(PSI),Variance=var(PSI), StandardDeviation=sd(PSI))
+
+# Create lot_summary dataframe
+lot_summary <- SusCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI),Variance=var(PSI), StandardDeviation=sd(PSI))
